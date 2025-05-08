b4ffbf70ddb63c2e41a3c99d26765369802825ab
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2025-05-08 17:40:23 -0500

    function examples!

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("hello everyone!")
+
+function howOld(year, name, state) {
+    // code goes here
+    console.log("You are " + (2025 - year) + " years old, your name is " + name + " and you live in " + state + ".");
+}
+
+let a = 1985;
+let b = "Chris";
+let c = "Texas";
+
+howOld(a, b, c)
+
+const people = ["Alice", "Suzy", "Bob"]
+
+function logNames(people) {
+    // code goes here
+    console.log("The names are: " + people.join(", ") + ".");
+}
+
+logNames(people)
